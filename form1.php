155adaf74974bd1a528b06351e24e9f8ff30774b
pravinrcp <pravinrcp3@gmail.com>
2018-01-29 12:14:03 +0530

    first changes

--- form1.php
+++ form1.php
@@ -5,7 +5,18 @@
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
-</form>
 
+
+<?php
+if(isset($_GET["name"])){
+    echo "<p>Hi, " . $_GET["name"] . "</p>";
+}
+?>
+<form method="get" action="<?php echo $_SERVER["PHP_SELF"];?>">
+    <label for="inputName">Name:</label>
+    <input type="text" name="name" id="inputName">
+    <input type="submit" value="Submit">
+</form>
+</body>
 </body>
 </html>
